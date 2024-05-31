1636a47e5c59809eeac8ecc5b65e0bc955073715
Phatdat01 <phatdatlht2001@gmail.com>
2024-06-01 01:16:12 +0700

    dat: remove demo file

--- demi.py
+++
@@ -1,9 +0,0 @@
-import tkinter  as tk 
-from tkcalendar import DateEntry
-my_w = tk.Tk()
-my_w.geometry("380x200")  
-
-cal=DateEntry(my_w,date_pattern="dd/mm/yyyy")
-cal.grid(row=1,column=1,padx=20)
-
-my_w.mainloop()
