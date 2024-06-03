fa6f185b4f913fbeccce54a4d4ff8744dc452c9d
Phatdat01 <phatdatlht2001@gmail.com>
2024-06-03 16:55:15 +0700

    dat: add load iteam 1 page

--- Crawl/crawling.py
+++ Crawl/crawling.py
@@ -13,8 +13,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
+from Crawl.storing import get_by_latest_file, move_to_des
+
 CONTENT_LIST = ["cn","chuyển nhượng"]
-FILE_LIST = ["Giấy chứng nhận","Tờ khai lệ phí trước bạ nhà, đất"]
+FILE_LIST = ["giấy chứng nhận","tờ khai lệ phí trước bạ nhà, đất"]
 
 def load_edge(download_path: str):
     prefs = {
@@ -101,26 +103,38 @@ def check_current_page(driver: WebDriver) -> List[int]:
     return [end_range, int(last.text)]
 
 
+def change_page(driver: WebDriver):
+    num =0
+    time.sleep(2.3)
+    while True:
+        try:
+            next = wait_element(driver=driver, timeout=2, key="//*[@title='Trang sau']", by="xpath")
+            next.click()
+            break
+        except:
+            if num>10:
+                break
+            time.sleep(1.3)
+
+
 def change_to_current_page(driver: WebDriver, page: int, limit_top: int):
-    time.sleep(0.7)
+    time.sleep(1.3)
     # show top x each page
     top = wait_element(driver=driver, timeout=30, key="ui.selection.floating.dropdown.w-auto", by="class")
     top.click()
-    time.sleep(0.7)
+    time.sleep(1.3)
     top_x = top.find_element(By.XPATH, value=f"//*[@data-value='{limit_top}']")
     top_x.click()
 
-    time.sleep(0.7)
+    time.sleep(1.3)
     # Change to current
     end_range, last = check_current_page(driver=driver)
 
     while page > 1:
-        time.sleep(1.3)
         if end_range >= last:
             break
         page -= 1
-        next = driver.find_element(By.XPATH, value="//*[@title='Trang sau']")
-        next.click()
+        change_page(driver=driver)
         end_range += limit_top
         
 
@@ -164,25 +178,25 @@ def access(driver: WebDriver, cre: json):
     time.sleep(1.7)
     re_from = wait_element(driver=driver, timeout= 30, key="NgayNhanTu",by="name")
     re_from.clear()
-    time.sleep(0.7)
+    time.sleep(1.3)
     re_from.send_keys(cre['re_from'])
     del re_from
 
     re_to = wait_element(driver=driver, timeout= 30, key="NgayNhanDen",by="name")
     re_to.clear()
-    time.sleep(0.7)
+    time.sleep(1.3)
     re_to.send_keys(cre['re_to'])
     del re_to
 
     result_from = wait_element(driver=driver, timeout= 30, key="NgayHenTraTu",by="name")
     result_from.clear()
-    time.sleep(0.7)
+    time.sleep(1.3)
     result_from.send_keys(cre['result_from'])
     del result_from
 
     result_to = wait_element(driver=driver, timeout= 30, key="NgayHenTraDen",by="name")
     result_to.clear()
-    time.sleep(0.7)
+    time.sleep(1.3)
     result_to.send_keys(cre['result_to'])
     del result_to
 
@@ -196,28 +210,33 @@ def access(driver: WebDriver, cre: json):
     search.click()
 
 
-def click_to_save(driver: WebDriver, cre: json):
+def click_to_save(driver: WebDriver, cre: json, page: int, id: int):
+    root = cre["path"].replace("\\", "\\\\")
     time.sleep(1.7)
     info = wait_element(driver=driver, timeout=30, key="item.XTT", by="class")
     info.click()
     time.sleep(1.7)
+    container_element = wait_element(driver=driver, timeout=30, key="//*[@ng-repeat='item in $p.objThanhPhanHoSoKemTheos']", by="xpath")
     container_element = driver.find_elements(By.XPATH, value="//*[@ng-repeat='item in $p.objThanhPhanHoSoKemTheos']")
-    file = 0
+    num_file = 0
 
-    # click to needed download
+    # download basic
     for container in container_element:
         text = container.find_element(By.CLASS_NAME, value="text-wrap.ng-binding").text
         for file_name in FILE_LIST:
-            if file_name in text:
-                container.find_element(By.TAG_NAME, value="a").click()
-                driver.switch_to.window(driver.window_handles[0])
-                time.sleep(1)
+            if file_name in text.lower():
+                try:
+                    container.find_element(By.TAG_NAME, value="a").click()
+                    driver.switch_to.window(driver.window_handles[0])
+                    time.sleep(1)
+                except:
+                    pass
 
     time.sleep(cre['delay'])
 
     for i in range(1,3):
         download_file(driver=driver, id=i)
-        file += 1
+        num_file += 1
     time.sleep(1.7)
     for i in range(2):
         close_file(driver=driver)
@@ -234,42 +253,71 @@ def click_to_save(driver: WebDriver, cre: json):
     rows = wait_element(driver=driver, timeout=30, key="//*[@ng-repeat-start='item in $data.items']", by="xpath")
     rows = driver.find_elements(By.XPATH, value="//*[@ng-repeat-start='item in $data.items']")
 
+    # download get result
     for row in rows:
         text = row.find_elements(By.TAG_NAME, value="td")[2].text
         if "Bến Lức" in text:
-            files = row.find_elements(By.CLASS_NAME, value="m-n.pointer.large.red.file.outline.pdf.icon")
-            files[0].click()
-            for fi in files:
-                fi.click()
-                driver.switch_to.window(driver.window_handles[0])
-                time.sleep(1)
+            try:
+                files = row.find_elements(By.CLASS_NAME, value="m-n.pointer.large.red.file.outline.pdf.icon")
+                if files:
+                    for fi in files:
+                        fi.click()
+                        driver.switch_to.window(driver.window_handles[0])
+                        time.sleep(1)
 
-            for i in range(1,len(files)+1):
-                download_file(driver=driver, id=i)
-                file += 1
-            time.sleep(1.7)
-            for i in range(len(files)):
-                close_file(driver=driver)
+                    time.sleep(cre["delay"])
+                    for i in range(1,len(files)+1):
+                        download_file(driver=driver, id=i)
+                        num_file += 1
 
-    if  len(driver.window_handles) >1:
-        for window_handle in range (1, len(driver.window_handles)):
-            close_file(driver=driver, id=window_handle)
+                    time.sleep(1.7)
+                    for i in range(len(files)):
+                        close_file(driver=driver)
+                    
+                    time.sleep(1.7)
+                    if  len(driver.window_handles) >1:
+                        for window_handle in range (1, len(driver.window_handles)):
+                            close_file(driver=driver, id=window_handle)
+            except:
+                pass
 
-    print(file)
+    time.sleep(1)
+    srcs = get_by_latest_file(num=num_file, download_path=cre["path"])
+    
+    # move
+    for src in srcs:
+    
+        move_to_des(
+            root=root,
+            ward=cre["ward"],
+            page= str(page),
+            id=str(id),
+            file=src
+        )
 
 def download(driver: WebDriver, cre: json):
-    time.sleep(1.7)
-    data = wait_element(driver=driver, timeout= 30, key="danhSachHoSo",by="id")
-    row = data.find_elements(By.TAG_NAME, value="tr")
+    time.sleep(3.7)
+    end_range, last = check_current_page(driver=driver)
+    page = cre["page"]
+    while True:
+        time.sleep(1.7)
+        data = wait_element(driver=driver, timeout= 30, key="danhSachHoSo",by="id")
+        row = data.find_elements(By.TAG_NAME, value="tr")
 
-    ok=""
-    for i in range(1,len(row)):
-        txt = row[i].find_elements(By.CLASS_NAME, value= "ng-binding")[5].text
-        if ok=="done":
+        # check each row of page
+        for i in range(4,len(row)):
+            txt = row[i].find_elements(By.CLASS_NAME, value= "ng-binding")[5].text
+            for content in CONTENT_LIST:
+                if content in txt.lower():
+                    row[i].click()
+                    click_to_save(driver=driver, cre=cre, page=page, id=i)
+                    driver.switch_to.window(driver.window_handles[0])
+                    time.sleep(2.3)
+                    close = driver.find_element(By.CSS_SELECTOR, value='.close.icon[style=";"]')
+                    close.click()
+
+        if end_range >= last:
             break
-        for content in CONTENT_LIST:
-            if content in txt.lower():
-                row[i].click()
-                click_to_save(driver=driver, cre=cre)
-                ok="done"
-                break
+        else:
+            page += 1
+            change_page(driver=driver)
